7e2f760718d5582a9244c81aa07abf2933000dcf
DevMilim <devmilim@gmail.com>
2025-11-11 21:10:21 -0300

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
 //! # Biblioteca web criada totalmente em std
 //! # Exemplo de uso:
-//! `````` rust
-//! use milim_web::server;
-//! use milim_web::request::Method::*;
-//!
-//! fn main() {
-//!    let mut app = server();
-//!
-//!    app.route("/", |req, res, ctx| {
-//!        let query = req.get_query("name").unwrap_or("".to_string());
-//!        res.body(&format!("Valor da query name e: {}", query))
-//!    });
-//!
-//!    app.route("/id/:name", |req, res, ctx| {
-//!        res.body(&format!(
-//!            "O valor de name e: {}",
-//!            req.get_param("name").unwrap_or("".to_string())
-//!        ));
-//!    });
-//!
-//!    app.listen("127.0.0.1:3000")
-//!}
-//!
-//!
-//! ```
+//! ``` rust
+/*!
+use milim_web::request::Method::*;
+use milim_web::server;
+
+fn main() {
+    let mut app = server();
+
+    app.route("/", Get, |req, res, ctx| {
+        let query = req.get_query("name").unwrap_or("".to_string());
+        res.body(&format!("Valor da query name e: {}", query));
+    });
+
+    app.route("/id/:name", Get, |req, res, ctx| {
+        res.body(&format!(
+            "O valor de name e: {}",
+            req.get_param("name").unwrap_or("".to_string())
+        ));
+    });
+
+    app.listen("127.0.0.1:3000")
+}
+
+*/
 use crate::aplication::App;
 pub mod aplication;
 pub mod config;
