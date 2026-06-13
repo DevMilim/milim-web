83b96545d77b8262ff910a737af59b83d3928652
DevMilim <devmilim@gmail.com>
2026-06-13 10:24:07 -0300

    relembrando a forma que se manipula state

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,15 +1,23 @@
+use std::sync::Mutex;
+
 use milim_web::{context::Context, macros::handler, request::HttpRequest, run_app, server};
 
 #[handler]
-async fn hello(req: &HttpRequest, ctx: &Context) -> &'static str {
-    "Hello World!!"
+async fn hello(req: &HttpRequest, ctx: &Context) -> String {
+    let mut count = ctx.get_state::<Mutex<u32>>().unwrap().lock().unwrap();
+
+    *count += 1;
+
+    format!("Hello: {}", count)
 }
 
 fn main() {
     use milim_web::request::Method::*;
     let mut app = server();
 
-    app.route(Get, "/hello").handler(hello);
+    app.manage(Mutex::new(0u32));
+
+    app.route(Get, "/").handler(hello);
 
     run_app(|| async {
         let _ = app.listen("127.0.0.1:3000").await;
